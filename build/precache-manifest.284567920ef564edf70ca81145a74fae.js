9b08372b1871b9bdf25f3375b04dd37cdcf07c84
Alekseysuev <Alekseysuev@users.noreply.github.com>
2019-02-19 17:35:04 +0100

    new app

---
+++ build/precache-manifest.284567920ef564edf70ca81145a74fae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b0baf067d36e6b2b01ae",
+    "url": "/static/js/main.b0baf067.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "b0baf067d36e6b2b01ae",
+    "url": "/static/css/main.9ee4b53a.chunk.css"
+  },
+  {
+    "revision": "98c1310b554748f56efc2b6385c7d269",
+    "url": "/index.html"
+  }
+];
